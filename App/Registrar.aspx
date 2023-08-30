cf38714b3af713bad529fcacf116adfc8a19dfd0
José David F <fontalvomejiajosedavid54@gmail.com>
2023-08-30 14:13:28 -0500

    Formulario

---
+++ App/Registrar.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registrar.aspx.cs" Inherits="App.Registrar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Registrar Empleados </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="estilos.css" />
+
+</head>
+<body class="container bg-light">
+
+    <!-- Start Header form -->
+    <div class="text-center pt-5">
+        <img src="https://i.ibb.co/8cDgdFX/Logo.png" alt="network-logo" width="72" height="72" />
+        <h2>Formulario de Registro de Créditos </h2>
+    </div>
+
+    <form id="registrar" runat="server">
+
+
+        <fieldset>
+            <legend><span class="number">1</span> Información </legend>
+
+            
+            <label>Id:</label>
+            <asp:Label ID="txt_id" runat="server" CssClass="id"></asp:Label>
+           
+            <label>Nombres:</label>
+            <asp:Label ID="txt_nombre" runat="server" CssClass="nombre"></asp:Label>
+        
+            <label>Apellidos:</label>
+            <asp:Label ID="txt_apellido" runat="server" CssClass="apellido"></asp:Label>
+        
+            <label>Contacto:</label>
+            <asp:Label ID="txt_contacto" runat="server" CssClass="contacto"></asp:Label>
+
+            <label>Correo:</label>
+            <asp:Label ID="txt_correo" runat="server" CssClass="correo"></asp:Label>
+
+            <label>Dirección:</label>
+            <asp:Label ID="txt_direccion" runat="server" CssClass="direccion"></asp:Label>
+
+            <label>Salario:</label>
+            <asp:Label ID="txt_salario" runat="server" CssClass="salario"></asp:Label>
+
+            <label>Empresa:</label>
+            <asp:Label ID="txt_empresa" runat="server" CssClass="empresa"></asp:Label>
+
+        </fieldset>
+        <asp:Button ID="btn_guardar" runat="server" Text="Guardar" />
+     
+    </form>
+
+
+
+</body>
+</html>
---
+++ App/Registrar.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace App
+{
+
+
+    public partial class Registrar
+    {
+
+        /// <summary>
+        /// Control registrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm registrar;
+
+        /// <summary>
+        /// Control txt_id.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_id;
+
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_nombre;
+
+        /// <summary>
+        /// Control txt_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_apellido;
+
+        /// <summary>
+        /// Control txt_contacto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_contacto;
+
+        /// <summary>
+        /// Control txt_correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_correo;
+
+        /// <summary>
+        /// Control txt_direccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_direccion;
+
+        /// <summary>
+        /// Control txt_salario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_salario;
+
+        /// <summary>
+        /// Control txt_empresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txt_empresa;
+
+        /// <summary>
+        /// Control btn_guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_guardar;
+    }
+}
